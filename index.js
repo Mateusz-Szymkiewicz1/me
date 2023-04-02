fe589a44d89dfea533e10817981b8fe29db8a73a
unknown <szymkiewiczmateusz1@gmail.com>
2023-04-02 22:49:08 +0200

    Lighthouse ;(

--- index.js
+++ index.js
@@ -8,19 +8,17 @@ document.querySelector("#btn_max").addEventListener("click", function () {
 })
 function responsivity(x) {
     if (x.matches) {
-        document.querySelector("#nav_kontakt").innerHTML = '<i class="fa fa-envelope"></i>';
-        document.querySelector("#nav_portfolio").innerHTML = '<i class="fa fa-briefcase"></i>';
-        document.querySelector("#nav_umiej").innerHTML = '<i class="fa fa-book"></i>';
-        document.querySelector("#nav_dosw").innerHTML = '<i class="fa fa-clipboard"></i>';
-        document.querySelector("#nav_omnie").innerHTML = '<i class="fa fa-user-tie"></i>';
-        document.querySelector("#nav_portfolio").style.paddingTop = "6px";
+        document.querySelector("#nav_kontakt a").innerHTML = '<i class="fa fa-envelope"></i>';
+        document.querySelector("#nav_portfolio a").innerHTML = '<i class="fa fa-briefcase"></i>';
+        document.querySelector("#nav_umiej a").innerHTML = '<i class="fa fa-book"></i>';
+        document.querySelector("#nav_dosw a").innerHTML = '<i class="fa fa-clipboard"></i>';
+        document.querySelector("#nav_omnie a").innerHTML = '<i class="fa fa-user-tie"></i>';
     } else {
-        document.querySelector("#nav_kontakt").innerHTML = '<span style="text-decoration:underline;">K</span>ontakt';
-        document.querySelector("#nav_portfolio").innerHTML = '<span style="text-decoration:underline;">P</span>ortfolio';
-        document.querySelector("#nav_umiej").innerHTML = '<span style="text-decoration:underline;">U</span>miejętności';
-        document.querySelector("#nav_dosw").innerHTML = '<span style="text-decoration:underline;">D</span>oświadczenie';
-        document.querySelector("#nav_omnie").innerHTML = '<span style="text-decoration:underline;">O</span> mnie';
-        document.querySelector("#nav_portfolio").style.paddingTop = "5px";
+        document.querySelector("#nav_kontakt a").innerHTML = 'Kontakt';
+        document.querySelector("#nav_portfolio a").innerHTML = 'Portfolio';
+        document.querySelector("#nav_umiej a").innerHTML = 'Umiejętności';
+        document.querySelector("#nav_dosw a").innerHTML = 'Doświadczenie';
+        document.querySelector("#nav_omnie a").innerHTML = 'O mnie';
     }
 }
 var x = window.matchMedia("(max-width: 590px)");
